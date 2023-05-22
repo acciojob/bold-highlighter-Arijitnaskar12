9681ee3ea01e0dbf3069f57a588bec7cb1b92620
Arijitnaskar12 <117929311+Arijitnaskar12@users.noreply.github.com>
2023-05-22 17:58:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+	let selectElement=document.getElementsByTagName("strong");
+
 function highlight() {
     //Write your code here
+	let selectElement=document.getElementsByTagName("strong");
+	for(let i=0;i<selectElement.length;i++)
+		{
+			selectElement[i].style.color="green";
+		}
 
 
 }
@@ -7,6 +14,9 @@ function highlight() {
 
 function return_normal() {
     //Write your code here
-
+for(let i=0;i<selectElement.length;i++)
+		{
+			selectElement[i].style.color="black";
+		}
     
 }
